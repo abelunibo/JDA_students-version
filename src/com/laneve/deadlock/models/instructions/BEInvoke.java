ff0822e14c7bc95ff3a934d5e6b6427fb485ee38
Marco Tiseo <t6o.mrc@gmail.com>
2015-05-24 16:55:41 +0200

    istruzione invokespecial <initi>

--- src/com/laneve/deadlock/models/instructions/BEInvoke.java
+++ src/com/laneve/deadlock/models/instructions/BEInvoke.java
@@ -2,13 +2,16 @@ package com.laneve.deadlock.models.instructions;
 
 import java.util.logging.Logger;
 
+import com.laneve.deadlock.exceptions.BEException;
 import com.laneve.deadlock.models.BEInstructionLine;
 import com.laneve.deadlock.models.Environment;
 import com.laneve.deadlock.models.lam.LamBase;
 import com.laneve.deadlock.models.lam.LamZT;
+import com.laneve.deadlock.type.Type;
+import com.laneve.deadlock.type.TypeObject;
 
 public class BEInvoke extends BEInstructionLine implements BEInstruction{
-	private String signature;
+	private String signature="";
 	private static Logger LOGGER = Logger.getLogger(BEInvoke.class.getSimpleName());
 	
 	public BEInvoke(String text) {
@@ -20,10 +23,11 @@ public class BEInvoke extends BEInstructionLine implements BEInstruction{
 		LamBase lzt = new LamZT();		
 		LamZT.addDebugZT(environment);
 		String lamZ = LamZT.getZhatBar(environment.getLocks());
-		String lamT = LamZT.getThat(environment.getQueuethreads());	
-		lzt.setLam(lamZ+" & "+lamT);
-		//		Logger.logInfo(lzt.getLam());
+		String lamT = LamZT.getThat(environment.getQueuethreads());
 		changeEnvironment(environment);		
+
+		lzt.setLam(lamZ+" & "+lamT+" & "+signature);
+		//		Logger.logInfo(lzt.getLam());
 		return lzt;
 	}
 
@@ -33,7 +37,8 @@ public class BEInvoke extends BEInstructionLine implements BEInstruction{
 		int numParameters=0;
 
 		if(getName().contentEquals("invokespecial")){
-			//			Logger.logInfo(environment.takeCpoolRef(getRef()));
+			//Logger.logInfo(environment.takeCpoolRef(getRef()));
+			
 			String signature = environment.takeCpoolRef(getRef());
 			int openP = signature.indexOf("(");
 			int closedP = signature.indexOf(")");
@@ -48,9 +53,6 @@ public class BEInvoke extends BEInstructionLine implements BEInstruction{
 				}
 				//Logger.logInfo(String.valueOf(numParameters));
 			}
-
-			LOGGER.info(environment.takeCpoolRef(getRef()));
-			
 		}
 		else if(getName().contentEquals("invokevirtual")){
 
@@ -59,16 +61,36 @@ public class BEInvoke extends BEInstructionLine implements BEInstruction{
 		else if(getName().contentEquals("invokestatic")){
 
 		}
+	    
+		//System.out.println(signature);
 
-//		try {
-//			for(int i = 0; i<numParameters; i++)
-//				signature = environment.popStack();
-//			
-//		} catch (BEException e) {
-//			e.printStackTrace();
-//			System.exit(1);
-//		}
+		//TODO solo per debug
+		Type uno = new TypeObject("P2");
+		Type due = new TypeObject("P1");
+		Type tre = new TypeObject("java/lang/Object");
+		environment.pushStack(tre);
+		environment.pushStack(due);
+		environment.pushStack(uno);
 
+		try {
+		    StringBuffer a = new StringBuffer(signature);
+		    //TODO solo per debug numParameters  
+		    // viene preso prima 
+		    numParameters = 2;//P1 //P2 
+			for(int i = 0; i<numParameters; i++){
+				a.insert(0,environment.popStack().getName()+",");
+			}
+		    a.insert(0,"(");
+		    a.insert(1,environment.popStack().getName()+",");
+		    a.deleteCharAt(a.length()-1);
+		    a.insert(a.length(),")");
+			a.insert(0,"<init> ");
+			signature = a.toString();
+
+			//System.out.println(a);
+		} catch (BEException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
-
 }
