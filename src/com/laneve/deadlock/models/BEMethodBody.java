cd5611026f5787a7c61fe0cf65d3a94f714869a6
Marco Tiseo <t6o.mrc@gmail.com>
2015-05-24 17:02:56 +0200

    gestione invokespecial

--- src/com/laneve/deadlock/models/BEMethodBody.java
+++ src/com/laneve/deadlock/models/BEMethodBody.java
@@ -11,6 +11,8 @@ public class BEMethodBody extends BEBase{
 	BEMethodModifier methodModifier=null;
 	BEMethodHeader methodHeader=null;
 
+	//TODO istruzioneTemp = vecchia istruzione
+
 	public BEMethodBody(LinkedList<BEInstructionLine> instructions) {
 		this.instructions = instructions;
 	}
@@ -18,22 +20,22 @@ public class BEMethodBody extends BEBase{
 	public LinkedList<BEInstructionLine> getInstructions() {
 		return instructions;
 	}
-	
+
 	public void setMethodSignature(BEMethodModifier methodModifier,
 			BEMethodHeader methodHeader) throws BEException{
-		
+
 		if(this.methodHeader!=null){
 			throw new BEException("La signature di un methodBody non puo' essere modificata");
 		}
-		
+
 		if(methodHeader==null){
 			throw new BEException("La signature di un methodBody deve avere methodHeader");
 		}
-		
+
 		this.methodModifier=methodModifier;
 		this.methodHeader=methodHeader;
 	}
-	
+
 	public BEMethodHeader getMethodHeader() throws BEException{
 		return methodHeader;
 	}
@@ -41,13 +43,38 @@ public class BEMethodBody extends BEBase{
 	public BEMethodModifier getMethodModifier() {
 		return methodModifier;
 	}
-	
+
+	//TODO getIstruzionePrecedente 
+	//TODO setIstruzioneSuccessivaGoto
+
+
 	@Override
 	public LamBase generateLam(Environment environment) {
 		LamSequence l = new LamSequence();
 		environment.openScope(this);
-		for(BEInstructionLine i : instructions)
+		//TODO for(int i = 0 ; i < instructions.size ;i++)
+		//TODO i = istruzionesuccessiva;
+		for(BEInstructionLine i : instructions){
+			 /*System.out.println(i.getName()+"--");
+			System.out.print("Stack--> "+ environment.getOperandStack().toString());
+			  
+			Iterator it = environment.getLocalVar().keySet().iterator();
+			  
+			System.out.print("\nLocalVar-> ");
+
+			while (it.hasNext()) {
+			   String key = it.next().toString();
+			   String value = environment.getLocalVar().get(key).getName();
+			   System.out.print(key + " " + value);
+			   System.out.print(" , ");
+			}
+			
+			System.out.println();*/
+			//TODO if setIstruzioneSuccessiva == null{
 			l.createSequence(i.generateLam(environment)); 
+			//TODO istruzioneTemp = i;
+		}
+		
 		environment.closeScope();
 		return l;
 	}
